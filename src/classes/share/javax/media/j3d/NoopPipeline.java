206aecb67ca35c180e894ed1c6dd92a1a2d8062d
Kevin Rushforth <kcr@dev.java.net>
2006-04-26 20:32:20 +0000

    Added copyright header
    git-svn-id: https://svn.java.net/svn/j3d-core~svn/branches/dev-1_5@578 ba19aa83-45c5-6ac9-afd3-db810772062c

--- src/classes/share/javax/media/j3d/NoopPipeline.java
+++ src/classes/share/javax/media/j3d/NoopPipeline.java
@@ -1,5 +1,13 @@
 /*
- * NoopPipeline
+ * $RCSfile$
+ *
+ * Copyright (c) 2006 Sun Microsystems, Inc. All rights reserved.
+ *
+ * Use is subject to license terms.
+ *
+ * $Revision$
+ * $Date$
+ * $State$
  */
 
 package javax.media.j3d;
