0f6b52956769920dfa95c0c362c4d663f0225cf2
Harvey Harrison <harvey.harrison@gmail.com>
2011-07-16 22:11:54 -0700

    java3d: fill in enough of VersionInfo.java to get it compiling
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>

---
+++ src/javax/media/j3d/VersionInfo.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2011 Harvey Harrison <harvey.harrison@gmail.com>
+ */
+package javax.media.j3d;
+
+// just filling in enough to get things compiling
+public class VersionInfo {
+	static final boolean isDebug = false;
+	static final boolean isDevPhase = false;
+	static final boolean isProduction = true;
+
+	static String getVendor() {
+		return "";
+	}
+
+	static String getSpecificationVendor() {
+		return "";
+	}
+
+	static String getVersion() {
+		return "1.6.0-devel";
+	}
+
+	static String getSpecificationVersion() {
+		return "1.6.0";
+	}
+}
