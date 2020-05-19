baff3df05ba098e8ae6de40fbf0e5568d1549979
Harvey Harrison <harvey.harrison@gmail.com>
2020-05-19 09:41:10 -0700

    J3D: add missing license header to GeometryService
    Match the licensing of the rest of the Java3D core.
    
    Signed-off-by: Harvey Harrison <harvey.harrison@gmail.com>

--- src/javax/media/j3d/GeometryService.java
+++ src/javax/media/j3d/GeometryService.java
@@ -1,4 +1,19 @@
-
+/*
+ * Copyright (c) 2020 JogAmp Community. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ */
 package javax.media.j3d;
 
 import java.util.ArrayList;
